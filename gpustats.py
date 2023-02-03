8619913224e7f641568695cd9d3d106fbd533ba1
slush0 <marek@palatinus.cz>
2023-02-03 18:21:15 +0000

    Adding temperature stats Make nvidia-smi parsing more readable

--- gpustats.py
+++ gpustats.py
@@ -7,25 +7,25 @@ import sqlite3
 from filelock import FileLock, Timeout
 
 DELAY=10 # seconds
+DEBUG=False
 
 def prepare_db(cur):
     cur.execute("""CREATE TABLE IF NOT EXISTS gpu
                     (timestamp INT, gpuid INT,
                     power_use INT, power_max INT,
                     mem_alloc INT, mem_total INT,
-                    util INT, fan INT)""")
+                    util INT, fan INT, temp INT)""")
     cur.execute("""CREATE TABLE IF NOT EXISTS proc
                     (timestamp INT, gpuid INT, pid INT,
                     mem_alloc INT, user CHAR)""")
+def store_gpu(gpu: list, timestamp: int, cur: any):
+    cur.executemany(f"""INSERT INTO gpu VALUES({timestamp},
+        :gpuid, :power_use, :power_max,
+        :mem_alloc, :mem_total, :util, :fan, :temp)""", gpu)
 
 def store_proc(proc: list, timestamp: int, cur: any):
     cur.executemany(f"""INSERT INTO proc VALUES({timestamp},
         :gpuid, :pid, :mem_alloc, :user)""", proc)
-
-def store_gpu(gpu: list, timestamp: int, cur: any):
-    cur.executemany(f"""INSERT INTO gpu VALUES({timestamp},
-        :gpuid, :power_use, :power_max,
-        :mem_alloc, :mem_total, :util, :fan)""", gpu)
 
 def gpustats():
     """Original idea https://github.com/serengil/gpuutils"""
@@ -63,36 +63,35 @@ def gpustats():
             if "MiB" not in line:
                     continue
 
-            power_info = line.split("|")[1]
-            power_capacity = int(power_info.split("/")[-1].replace("W", ""))
-            power_usage = int((power_info.split("/")[-2]).strip().split(" ")[-1].replace("W", ""))
+            power_info = [ c for c in line.split("|")[1].split(' ') if c != '' ]
+            fan_info = int(power_info[0].replace('%', ''))
+            temp_info = int(power_info[1].replace('C', ''))
+            power_usage = int(power_info[3].replace("W", ""))
+            power_max = int(power_info[5].replace("W", ""))
 
             memory_info = line.split("|")[2].replace("MiB","").split("/")
-            utilization_info = int(line.split("|")[3].split("%")[0])
-
-            fan_info = [ c for c in line.split('|')[3].split(' ') if c != '' ][0]
-            fan_info = int(fan_info.replace('%', ''))
-
-            allocated = int(memory_info[0])
-            total_memory = int(memory_info[1])
-            available_memory = total_memory - allocated
+            util_info = int(line.split("|")[3].split("%")[0])
+            mem_alloc = int(memory_info[0])
+            mem_total = int(memory_info[1])
 
             item = {
                 'gpuid': gpu_idx,
                 'power_use': power_usage,
-                'power_max': power_capacity,
-                'mem_use_perc': round(100*int(allocated)/int(total_memory), 1),
-                'mem_alloc': allocated,
-                'mem_avail': available_memory,
-                'mem_total': total_memory,
-                'util': utilization_info,
+                'power_max': power_max,
+                'mem_use_perc': round(100*mem_alloc/mem_total),
+                'mem_alloc': mem_alloc,
+                'mem_total': mem_total,
+                'util': util_info,
                 'fan': fan_info,
+                'temp': temp_info,
             }
+
             gpus.append(item)
             gpu_idx = gpu_idx + 1
     except Exception as err:
         print("there are no GPUs on your system (", str(err), ")")
-
+        if DEBUG:
+            raise
 
     return (processes, gpus)
 
@@ -117,12 +116,17 @@ def main():
             store_gpu(gpu, int(start), db)
             db.commit()
 
-            #print("Request took %.03f sec" % (time.time() - start))
-            #pprint(proc)
-            #pprint(gpu)
+            if DEBUG:
+                print("Request took %.03f sec" % (time.time() - start))
+                print("Processes:")
+                pprint(proc)
+                print("GPUs:")
+                pprint(gpu)
 
         except Exception as e:
             print(e)
+            if DEBUG:
+                raise
 
         time.sleep(DELAY)
 
