095718fbfc5579a4e8633e73611e1e4623d5e854
slush0 <marek@palatinus.cz>
2023-02-03 23:29:20 +0000

    Adding watch view

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # gpustats
 
 Periodically scrape ```nvidia-smi``` and store detailed info to local sqlite3 database.
+
+## How to run
+
+The script maintains its lock, so it is safe to run it periodically.
+If other instance is running, the script finish instantly.
+
+For quick run without any hassle with systemd, let's add this into crontab:
+```
+0 *  * * * cd <gpustats workdir>/ && ./gpustats.py
+```
+
+Alternatively, you can use gpustats.service as systemd launcher.
+That will require additional system configuration like creating gpustats user,
+creating working directory for lock and database etc, as this isn't handled by
+the script.
--- gpustats.py
+++ gpustats.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import time
-import subprocess
-from pprint import pprint
 import psutil
 import sqlite3
+import argparse
+import datetime
+import subprocess
+from pprint import pprint
+from prettytable import PrettyTable
 from filelock import FileLock, Timeout
 
 DELAY=10 # seconds
+WATCH_HISTORY=6 # How many entries show at once
 DEBUG=False
 
 def prepare_db(cur):
@@ -95,7 +99,71 @@ def gpustats():
 
     return (processes, gpus)
 
-def main():
+def watch_stats(args):
+    def format_timestamp(f, v): return str(datetime.datetime.fromtimestamp(v))
+    def format_temp(f, v): return f"{v} ˚C"
+    def format_perc(f, v): return f"{v} %"
+    def format_mb(f, v): return f"{v} MB"
+    def format_watt(f, v): return f"{v} W"
+
+    def render_table(db, pt, table_name, limit):
+        db = db.execute(f"""
+                SELECT * FROM {table_name} WHERE timestamp IN (
+                    SELECT timestamp FROM {table_name} ORDER BY rowid DESC LIMIT {WATCH_HISTORY}
+                )
+        """)
+        pt.clear_rows()
+        pt.field_names = [tup[0] for tup in db.description]
+        pt.add_rows(db.fetchall())
+        print(pt)
+
+    def get_last_timestamp(db):
+        return db.execute("SELECT timestamp FROM gpu ORDER BY rowid DESC LIMIT 1").fetchone()[0]
+
+    db = sqlite3.connect("gpustats.db", isolation_level=None)
+    table_gpu = PrettyTable()
+    table_gpu.align = "r"
+    table_gpu.custom_format = {
+        'timestamp': format_timestamp,
+        'temp': format_temp,
+        'util': format_perc,
+        'fan': format_perc,
+        'power_use': format_watt,
+        'power_max': format_watt,
+        'mem_alloc': format_mb,
+        'mem_total': format_mb,
+    }
+
+    table_proc = PrettyTable()
+    table_proc.align = "r"
+    table_proc.custom_format = {
+        'timestamp': format_timestamp,
+        'mem_alloc': format_mb,
+    }
+
+    try:
+        while True:
+            # Resets terminal
+            # https://www.unix.com/os-x-apple-/279401-means-clearing-scroll-buffer-osx-terminal.html
+            print("\033c\033[3J\033[2J\033[0m\033[H")
+
+            # Sync with data gathering script
+            # 0.5 sec extra because of timestamp precision on seconds
+            last_timestamp = get_last_timestamp(db)
+            delay = DELAY - (time.time() - last_timestamp) + 0.5
+            delay = max(1, delay)
+            delay = min(DELAY, delay)
+            print(f"Latency: %.03f s, Sleep: %.03f s" % \
+                    (time.time() - last_timestamp, delay))
+
+            render_table(db, table_gpu, "gpu", 1)
+            render_table(db, table_proc, "proc", 1)
+
+            time.sleep(delay)
+    finally:
+        db.close()
+
+def gather_stats(args):
     try:
         fl = FileLock('.lock', timeout=1)
         fl.acquire()
@@ -104,31 +172,50 @@ def main():
         return
 
     print(f"Starting {__file__}...")
-    db = sqlite3.connect("gpustats.db")
+    db = sqlite3.connect("gpustats.db", isolation_level=None)
     prepare_db(db)
 
-    while True:
-        try:
-            start = time.time()
-            proc, gpu = gpustats()
+    try:
+        while True:
+            try:
+                proc, gpu = gpustats()
+                # int() floors the timestamp and breaks latency calculator in watcher
+                timestamp = round(time.time())
 
-            store_proc(proc, int(start), db)
-            store_gpu(gpu, int(start), db)
-            db.commit()
+                store_proc(proc, timestamp, db)
+                store_gpu(gpu, timestamp, db)
+                db.commit()
 
-            if DEBUG:
-                print("Request took %.03f sec" % (time.time() - start))
-                print("Processes:")
-                pprint(proc)
-                print("GPUs:")
-                pprint(gpu)
+                if DEBUG:
+                    print("Processes:")
+                    pprint(proc)
+                    print("GPUs:")
+                    pprint(gpu)
 
-        except Exception as e:
-            print(e)
-            if DEBUG:
-                raise
+            except Exception as e:
+                print(e)
+                if DEBUG:
+                    raise
 
-        time.sleep(DELAY)
+            time.sleep(DELAY)
+
+    finally:
+        db.close()
+
+def main():
+    parser = argparse.ArgumentParser()
+    cmds = parser.add_mutually_exclusive_group()
+    cmds.add_argument("-g", "--gather", action="store_true", default=True,
+            help="Keep gathering stats and store them to sqlite")
+    cmds.add_argument("-w", "--watch", action="store_true",
+            help="Watch latest values.")
+
+    args = parser.parse_args()
+
+    if args.watch:
+        watch_stats(args)
+    elif args.gather:
+        gather_stats(args)
 
 if __name__ == '__main__':
     main()
